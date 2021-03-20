6bf1bd3c96e0a0a5350a32abbb8cf65b975108fc
ayeletavr <ayelet.avraham@mail.huji.ac.il>
2021-03-20 12:08:19 +0200

    Stack seem to work

---
+++ Stack.py
@@ -0,0 +1,32 @@
+"""
+author: ayelet.avraham@mail.huji.ac.il
+"""
+
+class Stack:
+
+    def __init__(self, data):
+        """
+        Constructor
+        """
+        self.data = data
+        self.pointer = data
+        self.size = 1
+
+    def pop(self):
+        self.data = None
+        self.size -= 1
+        return self.pointer
+
+    def insert(self, data):
+        self.pointer = data
+        self.size += 1
+
+    def size(self):
+        return self.size
+
+if __name__ == "__main__":
+    stack = Stack(1)
+    stack.insert(4)
+    print(stack.pop())
+    print(stack.size)
+
