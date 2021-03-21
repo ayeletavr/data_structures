c401708a5d0e72495d717eb0d7c571948b16c977
ayeletavr <ayelet.avraham@mail.huji.ac.il>
2021-03-21 17:41:27 +0200

    LCA works

--- BST.py
+++ BST.py
@@ -81,6 +81,43 @@ class BST:
         else:
             y.right = node
 
+    def inOrder(self, node):
+        """
+        Traverse the tree in order
+        :return:
+        """
+        if node:
+            self.inOrder(node.left)
+            print(node.data)
+            self.inOrder(node.right)
+
+    def findPath(self, root, path, data):
+        """
+        Finds a path from root to a given node.
+        Stores is in a list, and returns true if path exists else false.
+        """
+        if not root:
+            return False
+        path.append(root.data) #path from root always start with root.
+        if root.data == data:
+            return True
+        if (root.left and self.findPath(root.left, path, data)) or (root.right and self.findPath(root.right, path, data)):
+            return True
+        path.pop()
+        return False
+
+    def findLCA(self, root, n1, n2):
+        path1 = []
+        path2 = []
+        if (not self.findPath(self.root, path1, n1) or not self.findPath(self.root, path2, n2)):
+            return -1
+        i = 0
+        while (i < len(path1) and i < len(path2)):
+            if path1[i] != path2[i]:
+                break
+            i += 1
+        return path1[i-1]
+
 if __name__ == "__main__":
     node1 = Node(1)
     node2 = Node(2)
@@ -91,3 +128,5 @@ if __name__ == "__main__":
     tree.insert(node2)
     tree.insert(node4)
     tree.printBST()
+    tree.inOrder(tree.root)
+    print(tree.findLCA(tree.root, 1, 2))
